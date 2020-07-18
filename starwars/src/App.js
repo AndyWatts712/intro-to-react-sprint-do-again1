ead49efcab1d4045bfa9e77a53a8e7fb43910933
megawatts68 <andrew.f.watts@gmail.com>
2020-07-19 01:55:13 +0700

    Data working, characters listed.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Characters from './components/Character';
 import './App.css';
 
 const App = () => {
@@ -8,10 +10,23 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [char, setChar] = useState()
 
+  useEffect(() => {
+  axios.get('https://rickandmortyapi.com/api/character')
+    .then((resp) => {
+      console.log(resp)
+      setChar(resp.data.results)
+    })
+    .catch(err => console.log(err))
+  }, [])
+  console.log('CHAR', char)
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {char && char.map((chr) => {
+        return(
+      <Characters char = {chr.name} />)})}
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,13 @@
 // Write your Character component here
+import React from 'react';
+
+function Characters(props) {
+    console.log('PROPS', props)
+    const { char } = props
+
+    return(
+        <h2>{char}</h2>
+    ) 
+}
+
+export default Characters;
